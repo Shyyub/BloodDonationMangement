e80d066272c4ed70c716543c2313945b9253903d
Shyyub <ahamedshyyub@gmail.com>
2022-09-12 16:59:28 +0530

    First Commit

---
+++ Core/IDonar.cs
@@ -0,0 +1,14 @@
+﻿using BloodDonationManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodDonationManagement.Core
+{
+    public interface IDonar
+    {
+        IEnumerable<SchoolModel> GetAll();
+        void AddDonar(SchoolModel schoolModel);
+    }
+}
---
+++ Migrations/20220911134325_InitialMigration1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BloodDonationManagement.Migrations
+{
+    public partial class InitialMigration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "schoolModels",
+                columns: new[] { "DonarId", "DonarAddress", "DonarCity", "DonarName" },
+                values: new object[] { 1, "Davangere", "Delhi", "Shyyub" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "schoolModels",
+                keyColumn: "DonarId",
+                keyValue: 1);
+        }
+    }
+}
